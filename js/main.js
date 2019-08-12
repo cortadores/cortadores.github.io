5d1d8f7b0045f164d24914d3eb57a324c630959b
Diego Núñez <nunez.guzman@gmail.com>
2019-08-11 19:17:31 -0500

    Ajustes
    -Se agregan 3 nuevos elementos
    -Se agrega favicon
    -Se agrega logo de navbar fixed
    -Se eliminan fuentes que hacáin request a google.
    
    by Diego Núñez @diego-nz
    
    Co-Authored-By: diego-nz <diego-nz@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -106,10 +106,10 @@ $(document).ready(function(){
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 50) {
-            $(".navbar-brand a").css("color","#fff");
+            // $(".navbar-brand a").css("color","#fff");
             $("#top-bar").removeClass("animated-header");
         } else {
-            $(".navbar-brand a").css("color","inherit");
+            // $(".navbar-brand a").css("color","inherit");
             $("#top-bar").addClass("animated-header");
         }
     });
@@ -150,6 +150,10 @@ $(".fancybox").fancybox({
         }
     }
 });
+
+$('figcaption h4 a').on('click', function(e){
+    e.preventDefault();
+})
 
 
 
